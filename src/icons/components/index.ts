e287b2bfd1975f62254e64dec5fca578750811f3
Giorgi Khatiashvili <57389796+giorgikh93@users.noreply.github.com>
2026-04-23 13:46:53 +0000

    replay icon (#88)

--- src/icons/components/index.ts
+++ src/icons/components/index.ts
@@ -90,6 +90,7 @@ export { default as PhoneIphone } from "./PhoneIphone";
 export { default as PublicOutlined } from "./PublicOutlined";
 export { default as PushPin } from "./PushPin";
 export { default as QrCode } from "./QrCode";
+export { default as Replay } from "./Replay";
 export { default as Report } from "./Report";
 export { default as ReportProblem } from "./ReportProblem";
 export { default as ReportProblemRound } from "./ReportProblemRound";
