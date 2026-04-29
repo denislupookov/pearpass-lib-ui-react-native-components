b56bced337457c76deaf39ef508d8b3244d38dc5
Denis Lupookov <denislupookov@gmail.com>
2026-04-29 23:05:13 +0800

    Made input scrollable for the mobile

---
+++ src/components/InputField/InputValue.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { html } from 'react-strict-dom';
+import { styles } from './InputField.styles';
+import { InputValueProps } from './types';
+
+export const InputValue = ({ inputRef, ...rest }: InputValueProps): React.ReactElement => {
+  const { disabled, readOnly, onClick } = rest;
+  const preventFocus = readOnly && onClick
+    ? (e: React.MouseEvent<HTMLInputElement>) => e.preventDefault()
+    : undefined;
+
+  return (
+    <html.input
+      {...rest}
+      ref={inputRef}
+      onClick={disabled || readOnly ? undefined : onClick}
+      onMouseDown={preventFocus}
+      style={[styles.input, disabled && styles.containerDisabled]}
+    />
+  );
+};
